ddc1e099fdd22af525b0e5593c403589c23b8389
ganji umamaheshwar <ganji.umamaheshwar.2004@gmail.com>
2024-11-20 17:51:08 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,37 @@
+# s='''
+
+# 1.credit
+# 2.debit
+# 3.mini statement
+# 4.exit
+# '''
+# name="uday"
+# password="123"
+# user_name=input("enter the username:")
+# passwords=input("enter the password:")
+# s='''
+
+# 1.credit
+# 2.debit
+# 3.mini statement
+# 4.exit
+# '''
+# Amount=10000
+# if name==user_name and passwords==password:
+#     print(s)
+# while True:
+#     option=int(input("enter the option:"))
+#     if option==1:
+#         credit_amount=float(input("enter the ammount"))
+#         print("Amount after credit:",Amount+credit_amount)
+#     elif option==2:
+#         debit_amount=float(input("enter the ammount"))
+#         print("Amount after debit:",Amount-debit_amount)
+#     elif option==3:
+#         print("Amount:",Amount)
+#     elif option==4:
+#         break
+# else:
+#     print("incorrect")
+
+
